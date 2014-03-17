d36ed6287e3e147e9b4e8d43e548c34b01633baa
qdljl <qd-ljl@163.com>
2014-03-17 13:51:53 +0800

    Initial Commit

---
+++ nichat/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  nichat
+//
+//  Created by David on 14-3-17.
+//  Copyright (c) 2014年 David. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
